5f87491f08eb164d409db3c65fc509999156203e
Diogo Aguilar <diogoaguilar89@gmail.com>
2018-11-02 16:03:56 -0300

    Implementação do simulador API hospital

--- Pos.Arquitetura.Simulador.IoT.API/Pos.Arquitetura.Simulador.IoT.Mock/Program.cs
+++ Pos.Arquitetura.Simulador.IoT.API/Pos.Arquitetura.Simulador.IoT.Mock/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace Pos.Arquitetura.Simulador.IoT.Mock
 {
     class Program
     {
+        private static readonly HttpClient clientIoTAws = new HttpClient();
+        private static readonly HttpClient clientApiHospital = new HttpClient();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Simulador de Paciente!");
+            var urlCoisasAws = "https://a27ytjunf797pi-ats.iot.us-east-1.amazonaws.com/things/";
+            var urlAPIHospital = "http://localhost:54033/api";
+            var sufixoMedidorCardiaco = "/MedidorCardiaco/shadow";
+            var sufixoMedidorPressao = "/MedidorPressao/shadow";
+            var sufixoSensorRespiracao = "/SensorRespiracao/shadow";
+
+            clientIoTAws.BaseAddress = new Uri(urlCoisasAws);
+
+            var resultadoMedidorCardiaco = clientIoTAws.GetAsync(sufixoMedidorCardiaco);
+            var resultadoMedidorPressao = clientIoTAws.GetAsync(sufixoMedidorPressao);
+            var resultadoSensorRespiracao = clientIoTAws.GetAsync(sufixoSensorRespiracao);
+
+            clientApiHospital.BaseAddress = new Uri(urlAPIHospital);
+
         }
     }
 }
