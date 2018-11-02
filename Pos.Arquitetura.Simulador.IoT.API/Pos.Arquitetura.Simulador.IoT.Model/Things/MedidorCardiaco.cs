07d35b0a9e78d77103780da36cb998b838e868a3
Diogo Aguilar <diogoaguilar89@gmail.com>
2018-11-02 14:58:03 -0300

    Criação inicial do projeto

---
+++ Pos.Arquitetura.Simulador.IoT.API/Pos.Arquitetura.Simulador.IoT.Model/Things/MedidorCardiaco.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pos.Arquitetura.Simulador.IoT.Model.Things
+{
+    public class MedidorCardiaco
+    {
+        public string CodigoMedidor { get; set; }
+        public int QuantidadeBatimentos { get; set; }
+        public TimeSpan Hora { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
---
+++ Pos.Arquitetura.Simulador.IoT.API/Pos.Arquitetura.Simulador.IoT.Model/Things/MedidorPressaoArterial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pos.Arquitetura.Simulador.IoT.Model.Things
+{
+    public class MedidorPressaoArterial
+    {
+        public string CodigoMedidor { get; set; }
+        public decimal Sistolica { get; set; }
+        public decimal Diastolica { get; set; }
+    }
+}
